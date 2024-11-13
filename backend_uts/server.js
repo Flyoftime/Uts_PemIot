a5e7df767388df621c4509d1bd1d2dfc069b3b8d
Flyoftime <mirsabanwar@gmail.com>
2024-11-13 19:20:07 +0700

    Fix

---
+++ backend_uts/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors({
+    origin: 'http://127.0.0.1:5500' 
+}));
+
+app.get('/', (req, res) => {
+    res.send("Silakan akses /data untuk mendapatkan data JSON.");
+});
+
+app.get('/data', (req, res) => {
+    const data = {
+        suhumax: 36,
+        suhumin: 21,
+        suhurata: 28.35,
+        nilai_suhu_max_humid_max: [
+            {
+                idx: 101,
+                suhu: 36,
+                humid: 36,
+                kecerahan: 25,
+                timestamp: "2010-09-18 07:23:48"
+            },
+            {
+                idx: 226,
+                suhu: 36,
+                humid: 36,
+                kecerahan: 27,
+                timestamp: "2011-05-02 12:29:34"
+            }
+        ],
+        month_year_max: [
+            { month_year: "9-2010" },
+            { month_year: "5-2011" }
+        ]
+    };
+
+    res.json(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
